1c73fcf85b80666ec39367edac5bfe11598b87a9
packer97 <serovstanislav@yandex.ru>
2021-04-20 20:58:32 +0400

    #1: First commit.

---
+++ src/main/java/com/group_3/kanbanboard/KanbanBoardApplication.java
@@ -0,0 +1,13 @@
+package com.group_3.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group_3/kanbanboard/KanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_3.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
