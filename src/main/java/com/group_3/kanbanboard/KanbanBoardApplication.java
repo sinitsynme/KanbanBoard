83b06dc4effaae17d5a3bcf235cf7127db45a486
packer97 <serovstanislav@yandex.ru>
2021-04-21 22:30:34 +0400

    Add UserController

--- src/main/java/com/group_3/kanbanboard/KanbanBoardApplication.java
+++ src/main/java/com/group_3/kanbanboard/KanbanBoardApplication.java
@@ -1,6 +1,5 @@
 package com.group_3.kanbanboard;
 
-import com.group_3.kanbanboard.rest.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
