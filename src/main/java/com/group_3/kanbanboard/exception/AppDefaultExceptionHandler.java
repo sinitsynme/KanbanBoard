f0eb9add9d4b956222adc112f65219b6b3bb7a58
bloshound <bloshound@gmail.com>
2021-04-21 16:39:18 +0300

    #0.2 ExceptionController import fix

--- src/main/java/com/group_3/kanbanboard/exception/AppDefaultExceptionHandler.java
+++ src/main/java/com/group_3/kanbanboard/exception/AppDefaultExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.naming.AuthenticationException;
+import org.springframework.security.core.AuthenticationException;
 
 @ControllerAdvice
 public class AppDefaultExceptionHandler {
