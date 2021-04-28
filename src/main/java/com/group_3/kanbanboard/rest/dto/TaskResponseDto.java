055058fe78bdfbe32b4625b929048990c71c0179
bloshound <bloshound@gmail.com>
2021-04-28 20:07:53 +0300

    Fix TaskDTOs

--- src/main/java/com/group_3/kanbanboard/rest/dto/TaskResponseDto.java
+++ src/main/java/com/group_3/kanbanboard/rest/dto/TaskResponseDto.java
@@ -1,6 +1,31 @@
 package com.group_3.kanbanboard.rest.dto;
 
 
-public class TaskResponseDto {
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.UUID;
+
+public class  TaskResponseDto {
+
+    @Schema(description = "Название задачи")
+    private String title;
+    @Schema(description = "Категория задачи")
+    private String category;
+    @Schema(description = "Описание задачи")
+    private String description;
+    @Schema(description = "Статус задачи")
+    private String status;
+    @Schema(description = "Версия релиза")
+    private int releaseId;
+
+    public TaskResponseDto(String title, String category, String description, String status, int releaseId) {
+        this.title = title;
+        this.category = category;
+        this.description = description;
+        this.status = status;
+        this.releaseId = releaseId;
+    }
+
+    public TaskResponseDto() {
+    }
 }
