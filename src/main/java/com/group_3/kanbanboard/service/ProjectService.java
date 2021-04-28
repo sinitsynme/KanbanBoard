577b7d831a947b08897dc7a509211a7603c53ba7
Vadim Khundiryakov <hardstwin@gmail.com>
2021-04-28 05:08:13 +0300

    #9 Add ProjectService and ProjectServiceImpl

---
+++ src/main/java/com/group_3/kanbanboard/service/ProjectService.java
@@ -0,0 +1,20 @@
+package com.group_3.kanbanboard.service;
+
+import com.group_3.kanbanboard.rest.dto.ProjectRequestDto;
+import com.group_3.kanbanboard.rest.dto.ProjectResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProjectService {
+    ProjectResponseDto getById(UUID id);
+
+    List<ProjectResponseDto> getAllProjects();
+
+    ProjectResponseDto addProject(ProjectRequestDto projectRequestDto);
+
+    ProjectResponseDto updateUser(UUID id, ProjectRequestDto ProjectRequestDto);
+
+    void deleteProjectById(UUID id);
+
+}
