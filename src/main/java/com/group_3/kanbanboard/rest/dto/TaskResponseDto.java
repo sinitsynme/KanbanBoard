441381094cd13163e24378c3099e2775dfb3c8d2
bloshound <bloshound@gmail.com>
2021-04-29 15:59:29 +0300

    add migrations

--- src/main/java/com/group_3/kanbanboard/rest/dto/TaskResponseDto.java
+++ src/main/java/com/group_3/kanbanboard/rest/dto/TaskResponseDto.java
@@ -28,4 +28,44 @@ public class  TaskResponseDto {
 
     public TaskResponseDto() {
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getReleaseId() {
+        return releaseId;
+    }
+
+    public void setReleaseId(int releaseId) {
+        this.releaseId = releaseId;
+    }
 }
