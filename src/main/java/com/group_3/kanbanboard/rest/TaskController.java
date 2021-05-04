774ff2a05f8330fadf83c1841a3b69bce847c89b
bloshound <bloshound@gmail.com>
2021-05-04 22:18:36 +0300

    update TaskServiceImpl and TaskController

--- src/main/java/com/group_3/kanbanboard/rest/TaskController.java
+++ src/main/java/com/group_3/kanbanboard/rest/TaskController.java
@@ -2,8 +2,10 @@ package com.group_3.kanbanboard.rest;
 
 import com.group_3.kanbanboard.rest.dto.TaskRequestDto;
 import com.group_3.kanbanboard.rest.dto.TaskResponseDto;
+import com.group_3.kanbanboard.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,33 +16,43 @@ import java.util.UUID;
 @RequestMapping("api/tasks")
 @RestController
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
     @Operation(summary = "Получить список всех задач")
     @GetMapping()
     public ResponseEntity<List<TaskResponseDto>> getAllTasks() {
-        return null;
+        return ResponseEntity.ok().body(taskService.getAllTasks());
     }
 
     @Operation(summary = "Добавить задачу")
     @PostMapping()
     public ResponseEntity<TaskResponseDto> addTask(@RequestBody TaskRequestDto requestDto) {
-        return ResponseEntity.ok().body(new TaskResponseDto());
+        return ResponseEntity.ok().body(taskService.addTask(requestDto));
     }
 
     @Operation(summary = "Удалить задачу")
     @DeleteMapping("/{id}")
-    public ResponseEntity<TaskResponseDto> deleteTask(@PathVariable UUID id) {
+    public ResponseEntity<?> deleteTask(@PathVariable UUID id) {
+        taskService.deleteTask(id);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "получить задачу по id")
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponseDto> getTaskById(@PathVariable UUID id) {
-        return ResponseEntity.ok().body(new TaskResponseDto());
+        return ResponseEntity.ok().body(taskService.getById(id));
     }
 
     @Operation(summary = "обновить задачу")
     @PutMapping("/{id}")
     public ResponseEntity<TaskResponseDto> updateTask(@PathVariable UUID id, @RequestBody TaskRequestDto requestDto) {
-        return ResponseEntity.ok().body(new TaskResponseDto());
+        return ResponseEntity.ok().body(taskService.updateTask(id, requestDto));
     }
 }
--- src/main/java/com/group_3/kanbanboard/service/impl/TaskServiceImpl.java
+++ src/main/java/com/group_3/kanbanboard/service/impl/TaskServiceImpl.java
@@ -8,12 +8,14 @@ import com.group_3.kanbanboard.rest.dto.TaskRequestDto;
 import com.group_3.kanbanboard.rest.dto.TaskResponseDto;
 import com.group_3.kanbanboard.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
