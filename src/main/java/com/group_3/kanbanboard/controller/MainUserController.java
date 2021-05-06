b0c5bfc0092a7fd9fb102d62ed5ac26a30c902f2
bloshound <bloshound@gmail.com>
2021-05-06 10:27:29 +0300

    Add MainUserController and templates for users page.

--- src/main/java/com/group_3/kanbanboard/controller/MainUserController.java
+++ src/main/java/com/group_3/kanbanboard/controller/MainUserController.java
@@ -1,18 +1,45 @@
 package com.group_3.kanbanboard.controller;
 
-import com.group_3.kanbanboard.rest.UserController;
+import com.group_3.kanbanboard.rest.dto.UserRequestDto;
 import com.group_3.kanbanboard.rest.dto.UserResponseDto;
-import org.springframework.http.ResponseEntity;
+import com.group_3.kanbanboard.service.UserService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/users")
 public class MainUserController {
 
+    private final UserDetailsService userDetailsService;
+    private final UserService userService;
+
+    public MainUserController(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService, UserService userService) {
+        this.userDetailsService = userDetailsService;
+        this.userService = userService;
+    }
+
+
+    @GetMapping
+    public String usersList(Model model) {
+        model.addAttribute("usersList" , userService.getAllUsers());
+        return "usersList";
+    }
+
+
+    @GetMapping("/userDetail")
+    public String UserDetail(@RequestParam String userName, Model model) {
+       UserResponseDto userDto = new UserResponseDto();// - изменить
+       model.addAttribute("userDto", userDto);
+       return "userDetail";
+
+    }
 
 
 }
