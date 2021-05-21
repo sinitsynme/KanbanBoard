77ca7753b2d23bd3a11df88b56a646c1be02f54f
packer97 <serovstanislav@yandex.ru>
2021-05-21 18:45:58 +0400

    User list on project User page on project Controllers

--- src/main/java/com/group_3/kanbanboard/controller/UsersListOnProjectController.java
+++ src/main/java/com/group_3/kanbanboard/controller/UsersListOnProjectController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/project/users")
+@RequestMapping("/projects/users")
 public class UsersListOnProjectController {
 
     private final UserDetailsService userDetailsService;
