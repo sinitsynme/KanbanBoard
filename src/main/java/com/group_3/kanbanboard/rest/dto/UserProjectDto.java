3fe0f8584f5e53ee7bec3e35818732a7b78af7cf
Vadim Khundiryakov <hardstwin@gmail.com>
2021-05-21 20:33:50 +0300

    #24 Change ProjectRequestDto and ProjectResponseDto

---
+++ src/main/java/com/group_3/kanbanboard/rest/dto/UserProjectDto.java
@@ -0,0 +1,72 @@
+package com.group_3.kanbanboard.rest.dto;
+
+import com.group_3.kanbanboard.entity.ReleaseEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+import java.util.UUID;
+
+@Schema(description = "Проект юсера")
+public class UserProjectDto {
+    @Schema(description = "Название проекта")
+    private String title;
+    @Schema(description = "Описание проекта")
+    private String description;
+    @Schema(description = "Создатель проекта")
+    private UUID leadId;
+    @Schema(description = "Старт проекта")
+    private Boolean startProject;
+    @Schema(description = "Релизы")
+    private List<ReleaseEntity> releases;
+
+    public UserProjectDto() {
+    }
+
+    public UserProjectDto(String title, String description, UUID leadId, Boolean startProject, List<ReleaseEntity> releases) {
+        this.title = title;
+        this.description = description;
+        this.leadId = leadId;
+        this.startProject = startProject;
+        this.releases = releases;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UUID getLeadId() {
+        return leadId;
+    }
+
+    public void setLeadId(UUID leadId) {
+        this.leadId = leadId;
+    }
+
+    public Boolean getStartProject() {
+        return startProject;
+    }
+
+    public void setStartProject(Boolean startProject) {
+        this.startProject = startProject;
+    }
+
+    public List<ReleaseEntity> getReleases() {
+        return releases;
+    }
+
+    public void setReleases(List<ReleaseEntity> releases) {
+        this.releases = releases;
+    }
+}
